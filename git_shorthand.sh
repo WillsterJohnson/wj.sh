6d8e2f2800817152b2ad5df115b22f821f766d78
Will Johnson <willster@willsterjohnson.com>
2023-02-02 02:49:41 +0000

    feat: git remote shorthand

--- git_shorthand.sh
+++ git_shorthand.sh
@@ -6,6 +6,7 @@ function gtc () { git commit "$@";    };
 function gtcm() { git commit -m "$1"; };
 function gtpl() { git pull "$@";      };
 function gtpu() { git push "$@";      };
+function gtr () { git remote "$@";    };
 function gts () { git status "$@";    };
 function gtl () { git log "$@";       };
 function gtb () { git branch "$@";    };
@@ -18,6 +19,7 @@ function gthelp() {
   echo -e "  $(magenta "gtcm") $(grey "|") git commit -m $(grey "|") \`$(green "git commit -m {\$1}")\`";
   echo -e "  $(magenta "gtpl") $(grey "|") git pull      $(grey "|") \`$(green "git pull {\$@}")\`     ";
   echo -e "  $(magenta "gtpu") $(grey "|") git push      $(grey "|") \`$(green "git push {\$@}")\`     ";
+  echo -e "  $(magenta "gtr")  $(grey "|") git remote    $(grey "|") \`$(green "git remote {\$@}")\`   ";
   echo -e "  $(magenta "gts")  $(grey "|") git status    $(grey "|") \`$(green "git status {\$@}")\`   ";
   echo -e "  $(magenta "gtl")  $(grey "|") git log       $(grey "|") \`$(green "git log {\$@}")\`      ";
   echo -e "  $(magenta "gtb")  $(grey "|") git branch    $(grey "|") \`$(green "git branch {\$@}")\`   ";
